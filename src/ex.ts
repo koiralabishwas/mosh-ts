79033f146a6946a6a31092bb93a2e0ab034b6df5
BishwasKoirala <wasubisu69@gmail.com>
2023-12-20 15:45:04 +0900

    commit

--- src/ex.ts
+++ src/ex.ts
@@ -1,9 +1,50 @@
-type Value = unknown | 'a'
+// type Value = unknown | 'a'
 
-let value : Value ;
+// let value : Value ;
 
-if (typeof value === 'string') {
-  console.log(value.toUpperCase())
-} else {
-  console.log(typeof value)
+// if (typeof value === 'string') {
+//   console.log(value.toUpperCase())
+// } else {
+//   console.log(typeof value)
+// }
+
+class Logger {
+  constructor(
+    public logfile : string
+  ){}
+
+
+  log( message : string){}
+
+}
+
+class Person {
+  constructor(
+    public firstName : string,
+    public lastName : string
+  ) {}
+
+  get fullname () {
+    return `${this.firstName} ${this.lastName}`
+  }
+}
+
+class Employee extends Person{
+  constructor(
+    firstName : string,
+    lastName : string,
+    public salary : number
+  ){
+    super(firstName , lastName)
+  }
+}
+
+interface Employee {
+  name : string ,
+  salary : number ,
+}
+interface  address{
+  streets : string,
+  city : string ,
+  zipcode : number
 }
