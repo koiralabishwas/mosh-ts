396587d398d0a0ac98b1795006bb8af3d91b2882
Bishwas Koirala <wasubisu69@gmail.com>
2025-05-03 00:17:16 +0900

    difference between protected and private methods .
    private method => can be accessed in oly with the declared class . cannot be inherited nor are usable in object
    
    protected method => can be used inside inherited class , but cannot be used in objects

--- src/oop/inheritance.ts
+++ src/oop/inheritance.ts
@@ -5,50 +5,62 @@
 
 
 
-// class Person {
-//   constructor(
-//     public firstName : string ,
-//     public lastName : string
-//   ){}
+class Person {
+  constructor(
+    public firstName : string ,
+    public lastName : string
+  ){}
 
-//   get fullName(){
-//     return this.firstName +' ' + this.lastName
-//   }
-
-
-//   public walk() {
-//     console.log('waling')
-//   }
-// }
+  get fullName(){
+    return this.firstName +' ' + this.lastName
+  }
 
 
-// class Student extends Person {
-//   constructor(
-//     // need to add all properties including the inherited properties
-//     public studenId : number,
-//     firstName : string,
-//     lastName : string
-//   ){
-//     // call parent Class property inside super()
-//     super(firstName , lastName)
-//   }
+  public walk() {
+    console.log('waling')
+  }
 
-//   takeTest() {{
-//     console.log('Taking the tests')
-//   }}
-// }
+  private priv() {
+    console.log('can only be accessed within the class .not in object. also dont get inherited')
+  }
+
+  protected prot() {
+    console.log('can be accessed in inherited class . cannot be accessed in object or inherited object')
+  }
+}
 
 
-// let student = new Student(1 , "Bishwas" , "Koirala")
-// //  student. has all methods and properties from PersonClass + StudentClass
 
-// class Teacher extends Person {
-//   // can override getName() method from inherited Person Class
-//   override get fullName() {
-//     // return 'Proffesser' + this.firstName + this.lastName
-//     return 'Professer ' + super.fullName // can shortcut with this super 
-//   }
-// }
+class Student extends Person {
+  constructor(
+    // need to add all properties including the inherited properties
+    public studenId : number,
+    firstName : string,
+    lastName : string
+  ){
+    // call parent Class property inside super()
+    super(firstName , lastName)
+  }
 
-// let teacher = new Teacher("Johs" , "Smith")
-// console.log(teacher.fullName)
+  takeTest() {{
+    console.log('Taking the tests')
+    this.prot() // protected methods are inheritable
+  }}
+
+
+}
+
+
+let student = new Student(1 , "Bishwas" , "Koirala")
+//  student. has all methods and properties from PersonClass + StudentClass
+
+class Teacher extends Person {
+  // can override getName() method from inherited Person Class
+  override get fullName() {
+    // return 'Proffesser' + this.firstName + this.lastName
+    return 'Professer ' + super.fullName // can shortcut with this super 
+  }
+}
+
+let teacher = new Teacher("Johs" , "Smith")
+console.log(teacher.fullName)
--- src/oop/polymorphism.ts
+++ src/oop/polymorphism.ts
@@ -1,70 +1,70 @@
- class Person {
-  constructor(
-    public firstName : string ,
-    public lastName : string
-  ){}
+//  class Person {
+//   constructor(
+//     public firstName : string ,
+//     public lastName : string
+//   ){}
 
-  get fullName(){
-    return this.firstName +' ' + this.lastName
-  }
-
-
-  public walk() {
-    console.log('waling')
-  }
-}
+//   get fullName(){
+//     return this.firstName +' ' + this.lastName
+//   }
 
 
-class Student extends Person {
-  constructor(
-    // need to add all properties including the inherited properties
-    public studenId : number,
-    firstName : string,
-    lastName : string
-  ){
-    // call parent Class property inside super()
-    super(firstName , lastName)
-  }
-
-  takeTest() {{
-    console.log('Taking the tests')
-  }}
-}
+//   public walk() {
+//     console.log('waling')
+//   }
+// }
 
 
-class Teacher extends Person {
-  // can override getName() method from inherited Person Class
-  override get fullName() {
-    // return 'Proffesser' + this.firstName + this.lastName
-    return 'Professer ' + super.fullName // can shortcut with this super 
-  }
-}
+// class Student extends Person {
+//   constructor(
+//     // need to add all properties including the inherited properties
+//     public studenId : number,
+//     firstName : string,
+//     lastName : string
+//   ){
+//     // call parent Class property inside super()
+//     super(firstName , lastName)
+//   }
+
+//   takeTest() {{
+//     console.log('Taking the tests')
+//   }}
+// }
 
 
-class Principal extends Person {
-  override get fullName() {
-    return 'Principal ' + super.fullName // this super refers Teacher
-  }
-}
-
-/**
- *  ///open close principal
- *  Our code must be open to extenstion and close to modification
- *  ---> we should able to extend or inheri from them , not suitable to modify them
- */
-/////// polymorphism IN Action
-
-printNames([
-  new Student(1 , "John" , "Smith"),
-  new Teacher("Mosh" , "Hamedani"),
-  new Principal("Gede" , "Sasti")
-])
+// class Teacher extends Person {
+//   // can override getName() method from inherited Person Class
+//   override get fullName() {
+//     // return 'Proffesser' + this.firstName + this.lastName
+//     return 'Professer ' + super.fullName // can shortcut with this super 
+//   }
+// }
 
 
-// here , parameter excepts Person , we can also pass other class that inherits person
-function printNames(people : Person[]) {
-  for (let person of people) {
-    console.log(person.fullName)
-  }
-}
+// class Principal extends Person {
+//   override get fullName() {
+//     return 'Principal ' + super.fullName // this super refers Teacher
+//   }
+// }
+
+// /**
+//  *  ///open close principal
+//  *  Our code must be open to extenstion and close to modification
+//  *  ---> we should able to extend or inheri from them , not suitable to modify them
+//  */
+// /////// polymorphism IN Action
+
+// printNames([
+//   new Student(1 , "John" , "Smith"),
+//   new Teacher("Mosh" , "Hamedani"),
+//   new Principal("Gede" , "Sasti")
+// ])
+
+
+// // here , parameter excepts Person , we can also pass other class that inherits person
+// function printNames(people : Person[]) {
+//   for (let person of people) {
+//     console.log(person.fullName)
+//   }
+// }
 
