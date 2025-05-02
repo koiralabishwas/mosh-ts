e613ed0dfe47be436735bc1869fc3ca1ab3ad388
Bishwas Koirala <wasubisu69@gmail.com>
2025-05-03 00:11:20 +0900

    did polymorphism => if it takes type A , it also takes every other type/classes that inherits A

--- src/oop/inheritance.ts
+++ src/oop/inheritance.ts
@@ -1,47 +1,54 @@
-class Person {
-  constructor(
-    public firstName : string ,
-    public lastName : string
-  ){}
-
-  get fullName(){
-    return this.firstName +' ' + this.lastName
-  }
+/**
+ * エラー回避のためコメントアウトしている
+ */
 
 
-  public walk() {
-    console.log('waling')
-  }
-}
 
 
-class Student extends Person {
-  constructor(
-    // need to add all properties including the inherited properties
-    public studenId : number,
-    firstName : string,
-    lastName : string
-  ){
-    // call parent Class property inside super()
-    super(firstName , lastName)
-  }
+// class Person {
+//   constructor(
+//     public firstName : string ,
+//     public lastName : string
+//   ){}
 
-  takeTest() {{
-    console.log('Taking the tests')
-  }}
-}
+//   get fullName(){
+//     return this.firstName +' ' + this.lastName
+//   }
 
 
-let student = new Student(1 , "Bishwas" , "Koirala")
-//  student. has all methods and properties from PersonClass + StudentClass
+//   public walk() {
+//     console.log('waling')
+//   }
+// }
 
-class Teacher extends Person {
-  // can override getName() method from inherited Person Class
-  override get fullName() {
-    // return 'Proffesser' + this.firstName + this.lastName
-    return 'Professer ' + super.fullName // can shortcut with this super 
-  }
-}
 
-let teacher = new Teacher("Johs" , "Smith")
-console.log(teacher.fullName)
+// class Student extends Person {
+//   constructor(
+//     // need to add all properties including the inherited properties
+//     public studenId : number,
+//     firstName : string,
+//     lastName : string
+//   ){
+//     // call parent Class property inside super()
+//     super(firstName , lastName)
+//   }
+
+//   takeTest() {{
+//     console.log('Taking the tests')
+//   }}
+// }
+
+
+// let student = new Student(1 , "Bishwas" , "Koirala")
+// //  student. has all methods and properties from PersonClass + StudentClass
+
+// class Teacher extends Person {
+//   // can override getName() method from inherited Person Class
+//   override get fullName() {
+//     // return 'Proffesser' + this.firstName + this.lastName
+//     return 'Professer ' + super.fullName // can shortcut with this super 
+//   }
+// }
+
+// let teacher = new Teacher("Johs" , "Smith")
+// console.log(teacher.fullName)
