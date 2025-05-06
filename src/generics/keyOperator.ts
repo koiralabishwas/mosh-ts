2bd728c10b890ae09140184ff6c0e8fbcc41dcdf
Bishwas Koirala <wasubisu69@gmail.com>
2025-05-07 00:23:36 +0900

    can use key of operator to prevent pass nonexisting type

---
+++ src/generics/keyOperator.ts
@@ -0,0 +1,22 @@
+interface Product {
+  name : string , 
+  price :number
+}
+
+class SStore<T> {
+  protected objects : T[] = []
+
+  add(obj : T) : void {
+    this.objects.push(obj)
+  }
+
+  find(property : keyof T , value : unknown) : T | undefined {
+    return this.objects.find(obj => obj[property] === value)
+  }
+}
+
+let sstore = new SStore<Product>()
+sstore.add({name : "Bishas" , price : 999})
+sstore.find('name' , 'a')
+sstore.find('price' , 'a')
+sstore.find('preventpassingNoxExistingPropertyWithKEYOF' , 'a')
