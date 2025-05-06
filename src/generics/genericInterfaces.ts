c40501effef8d7a65078c3e0811fe82d1ee4f397
Bishwas Koirala <wasubisu69@gmail.com>
2025-05-06 17:52:14 +0900

    did generic interfaces , used them in funciton .

---
+++ src/generics/genericInterfaces.ts
@@ -0,0 +1,24 @@
+interface Result<T>{
+  data : T | null
+  error : string |null 
+}
+
+function fechhh<T>(url : string) : Result<T> {
+  return {data : null , error : null}
+}
+
+
+interface AUser {
+  userName : string 
+}
+
+interface Products {
+  titile : string 
+}
+
+let userResult = fechhh<AUser>("www.wtf.com")
+const userName = userResult.data?.userName
+
+let productResult = fechhh<Products>("www.products.com")
+const prdName = productResult.data?.titile
+
