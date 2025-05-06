926cb4e6aea84bc4cd6b581f6f7c4941f057c45d
Bishwas Koirala <wasubisu69@gmail.com>
2025-05-07 00:16:04 +0900

    need to revise generic classes again

--- src/generics/ExtendingGenericClass.ts
+++ src/generics/ExtendingGenericClass.ts
@@ -4,24 +4,39 @@ interface Prouduct {
 }
 
 class Store<T> {
-  private objects : T[] = []
+  protected objects : T[] = []
 
   add(obj : T) : void {
     this.objects.push(obj)
   }
 }
 
-class CompressibleStore<T> extends Store<T> {
-  compress (){}
-}
-
-// class SearchableStore<T>
-
-
 let store = new Store<Prouduct>()
 store.add({name : "onion" , price : 100})
 
 
+// pass on the generic type parameter from base class to child class 
+class CompressibleStore<T> extends Store<T> {
+  compress (){}
+}
 let comStore = new CompressibleStore()
 comStore.add({name : "tomato" , price : 200 })
 comStore.compress()
+
+
+// Restricting the generic type parameter 
+class SearchableStore<T extends {name : string }> extends Store<T> {
+  find(name : string) : T | undefined {
+    return this.objects.find((obj) => obj.name === name)
+  }
+}
+
+
+// Fix ? Terminate ?? the generic type parameter
+class ProuductStore extends Store<Prouduct> {
+  filterByCategory(category : string) : Prouduct[] {
+    return []
+  }
+}
+
+
---
+++ src/generics/TODO.md
@@ -0,0 +1,4 @@
+### TODO
+
+  - [x] redo generics
+  - [ ] mainly ExtendingGenericClass.ts
