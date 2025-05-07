5be1294d5440df4cebefc7cc577b3fefdeb18c0a
Bishwas Koirala <wasubisu69@gmail.com>
2025-05-07 13:50:36 +0900

    did ex

---
+++ src/generics/ex.ts
@@ -0,0 +1,13 @@
+function echo<T>(arg : T) : T {return arg}
+
+function printName<T extends {name : string}>(obj:T) {
+  console.log(obj.name)
+}
+
+class Entity <T>{
+  constructor(public id : T) {}
+}
+
+
+// key of User returns userId | userName
+
